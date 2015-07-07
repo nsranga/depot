8dd96fd2ef59026efd95dd294bf0b550f103fbbb
ranga <ranganadhan.nadadhur@gmail.com>
2015-07-07 22:01:17 +0530

    changed one message

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ActiveRecord::Base
 	validates :title, uniqueness: true 
 	validates :image_url, allow_blank: true, format: { 
 		with: %r{\.(gif|jpg|png)\Z}i, 
-		message: 'must be a URL for GIF, JPG or PNG image.' 
+		message: 'must be A URL for GIF, JPG or PNG image.' 
 	}
 	validates :title, length: {minimum: 10}
 end
